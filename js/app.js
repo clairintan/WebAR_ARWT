9c9f8383b6f356ec73946865b3f96dfbf98a6e92
clairintan <clairintan@gmail.com>
2025-04-18 02:39:45 +0800

    V1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/build.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
